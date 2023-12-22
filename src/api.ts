eff8ebec548795e37c6ca88cf0d9873360e8dec4
Vladislav Trofimenko <slashcooperlive@gmail.com>
2023-12-22 13:56:57 +0300

    init

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://back:8000',
   headers: {
     'Content-Type': 'application/json'
   },
